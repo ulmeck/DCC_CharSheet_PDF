997ebea0c938520fcf54ad1cab5c93754ef14250
ulmeck <ulm@appin.org>
2024-12-05 16:06:26 -0600

    Update level_0_stat.js

--- level_0_stat.js
+++ level_0_stat.js
@@ -32,3 +32,34 @@ if (fieldnum == "LUC") {
     }
 }
 
+
+
+
+var thisField = event.target.name;
+var target = this.getField(event.targetName).valueAsString.replace(/[^\d]/g, '');
+var instance = thisField.slice(-1);
+var fieldnum = thisField.slice(0,3);
+var agiBonus = "AGI-BONUS" + "-" + instance;
+var agiBonusNum = Number(this.getField(agiBonus).value);
+var luckBonus = "LUC-BONUS" + "-" + instance;
+var luckBonusNum = Number(this.getField(luckBonus).value);
+var luckBool = "Reflex-Luck" + "-" + instance;
+var luckBoolVal = this.getField(luckBool).value;
+
+bonus = agBonusNum;
+
+if (luckBoolVal == "Yes") {
+  bonus += luckBonusNum;
+}
+
+
+console.println("thisField=" + thisField);
+console.println("target=" + target);
+console.println("field=" + field);
+console.println("fieldnum=" + fieldnum);
+console.println("instance=" + instance);
+console.println("luckbonusnum=" + luckBonusNum);
+console.println("luckbonus=" + luckBonus);
+console.println("luckbool=" + luckBool);
+console.println("luckBoolVal=" + luckBoolVal);
+console.println("x="+ agBonusNum);
