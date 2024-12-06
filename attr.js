864ff590af2dcab1eda8bea78d7e16bd4de161ed
ulmeck <ulm@appin.org>
2024-12-05 19:48:35 -0600

    Create attr.js

---
+++ attr.js
@@ -0,0 +1,23 @@
+var thisField = event.target.name;
+var target = this.getField(event.targetName).valueAsString.replace(/[^\d]/g, '');
+var instance = thisField.slice(-1);
+var fieldnum = thisField.slice(0,3);
+var field = fieldnum;
+var fieldBonus = fieldnum + "-Bonus-Add";
+var attr = Number(this.getField(field).value);
+var bonus = Number(this.getField(fieldBonus).value)
+var mod = (((attr-10.5)*2/5) | 0) + bonus;
+
+if (fieldnum == "LUC") {
+
+  if (!!!target && !!attr) {
+    event.value = mod;
+  }  
+} else {
+    if (!!!attr) {  
+      event.value = "";
+    } else {
+	event.value = mod;
+    }
+}
+
