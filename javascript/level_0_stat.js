97fe901e03faf14d0a50b8138462cfa2d23e7ab8
ulmeck <ulm@appin.org>
2025-04-23 16:46:13 -0500

    updates
    embedded fonts in level 0 sheet

--- javascript/level_0_stat.js
+++ javascript/level_0_stat.js
@@ -1,3 +1,4 @@
+//ATTR, Debug version
 var thisField = event.target.name;
 var target = this.getField(event.targetName).valueAsString.replace(/[^\d]/g, '');
 var instance = thisField.slice(-1);
@@ -33,12 +34,28 @@ if (fieldnum == "LUC") {
 }
 
 
-
-
+//ATTR, Normal Version
 var thisField = event.target.name;
 var target = this.getField(event.targetName).valueAsString.replace(/[^\d]/g, '');
 var instance = thisField.slice(-1);
 var fieldnum = thisField.slice(0,3);
+var field = fieldnum + "-" + instance;
+var attr = Number(this.getField(field).value);
+
+var mod = (attr-10.5)*2/5 | 0;
+
+
+    if (!!!attr) {  
+      event.value = "";
+    } else {
+	event.value = mod;
+    }
+
+
+//Luck - Ref
+var thisField = event.target.name;
+var target = this.getField(event.targetName).valueAsString.replace(/[^\d]/g, '');
+var instance = thisField.slice(-1);
 var agiBonus = "AGI-BONUS" + "-" + instance;
 var agiBonusNum = Number(this.getField(agiBonus).value);
 var luckBonus = "LUC-BONUS" + "-" + instance;
@@ -46,11 +63,58 @@ var luckBonusNum = Number(this.getField(luckBonus).value);
 var luckBool = "Reflex-Luck" + "-" + instance;
 var luckBoolVal = this.getField(luckBool).value;
 
-bonus = agBonusNum;
+bonus = agiBonusNum;
+
 
 if (luckBoolVal == "Yes") {
   bonus += luckBonusNum;
 }
+
+event.value = bonus;
+
+//Luck - Fort
+var thisField = event.target.name;
+var target = this.getField(event.targetName).valueAsString.replace(/[^\d]/g, '');
+var instance = thisField.slice(-1);
+var staBonus = "STA-BONUS" + "-" + instance;
+var staBonusNum = Number(this.getField(staBonus).value);
+var luckBonus = "LUC-BONUS" + "-" + instance;
+var luckBonusNum = Number(this.getField(luckBonus).value);
+var luckBool = "Fort-Luck" + "-" + instance;
+var luckBoolVal = this.getField(luckBool).value;
+
+bonus = staBonusNum;
+
+
+if (luckBoolVal == "Yes") {
+  bonus += luckBonusNum;
+}
+
+event.value = bonus;
+
+
+//Luck - Will
+var thisField = event.target.name;
+var target = this.getField(event.targetName).valueAsString.replace(/[^\d]/g, '');
+var instance = thisField.slice(-1);
+var willBonus = "PER-BONUS" + "-" + instance;
+var willBonusNum = Number(this.getField(willBonus).value);
+var luckBonus = "LUC-BONUS" + "-" + instance;
+var luckBonusNum = Number(this.getField(luckBonus).value);
+var luckBool = "Will-Luck" + "-" + instance;
+var luckBoolVal = this.getField(luckBool).value;
+
+bonus = willBonusNum;
+
+
+if (luckBoolVal == "Yes") {
+  bonus += luckBonusNum;
+}
+
+event.value = bonus;
+
+
+
 
 
 console.println("thisField=" + thisField);
